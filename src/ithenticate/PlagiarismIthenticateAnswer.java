59f328f9b51753fddf7e8a725b3b6b35188e77ba
pdouvitsas <p_douvitsas@hotmail.com>
2018-01-25 14:11:25 +0000

    Add files via upload

---
+++ src/ithenticate/PlagiarismIthenticateAnswer.java
@@ -0,0 +1,31 @@
+package ithenticate;
+
+
+
+public class PlagiarismIthenticateAnswer {
+	private String url;
+	private Integer ready;
+	
+	public PlagiarismIthenticateAnswer() {
+		
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getReady() {
+		return ready;
+	}
+
+	public void setReady(Integer ready) {
+		this.ready = ready;
+	}
+	
+	
+	
+}

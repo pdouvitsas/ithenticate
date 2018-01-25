8d6b56379d3c552f9aa00fac2b8075931ed99f57
pdouvitsas <p_douvitsas@hotmail.com>
2018-01-25 14:02:35 +0000

    Update FetchSimilarityReport.java

--- src/ithenticate/FetchSimilarityReport.java
+++ src/ithenticate/FetchSimilarityReport.java
@@ -19,7 +19,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import com.sp.journalweb.domain.PlagiarismIthenticateAnswer;
+import ithenticate.PlagiarismIthenticateAnswer;
 
 public class FetchSimilarityReport {
 
